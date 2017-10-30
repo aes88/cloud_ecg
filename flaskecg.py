aab2e5d9e84712c01bc3be9916115b16f442ee5a
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-10-30 17:40:37 -0400

    Shaky function written for the requests endpoint that may work?

--- flaskecg.py
+++ flaskecg.py
@@ -73,9 +73,9 @@ class HrmVals:
         self.tachy = tb_ecg.tachy
         self.brady = tb_ecg.brady
 
-countsum = [0]
-countave = [0]
 
+countave = 0
+counts = 0
 @app.route("/api/heart_rate/summary", methods=['POST'])
 def hrsummary():
     import numpy as np
@@ -120,7 +120,8 @@ def hrsummary():
     instant_hr = ecgcalcs.instant_hr
     tachycondition = ecgcalcs.tachy
     bradycondition = ecgcalcs.brady
-    countsum[0] += 1
+    global counts
+    counts += 1
     #for row in list(zip(time, instant_hr, tachycondition, bradycondition)):
     #    return("{},{},{},{}\n".format(np.round(row[0], 2),
     #                                  np.round(row[1], 2),
@@ -208,7 +209,8 @@ def hrmaverage():
     average_hr = ecgcalcs.average_hr
     tachycondition = ecgcalcs.tachy
     bradycondition = ecgcalcs.brady
-    countave[0] += 1
+    global countave
+    countave += 1
     return_message = {"averaging_period":average_window, "time": t,
                       "tachycardia_annotations": tachycondition,
                       "bradycardia_annotations": bradycondition}
@@ -216,5 +218,5 @@ def hrmaverage():
 
 @app.route("/api/requests",methods = ['GET'])
 def requests ():
-   totalhits = countsum[0] + countave[0]
-   return totalhits
+    totalhits = counts + countave
+    return jsonify(totalhits)
