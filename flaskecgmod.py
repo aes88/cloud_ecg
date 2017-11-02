258900a01e7a688ca590183fe3e4e7f7ddec77ca
Sameer Pandhare <sammyp@Sameers-MacBook-Pro.local>
2017-11-02 17:07:11 -0400

    Added modular version of hrm average endpoint

--- flaskecgmod.py
+++ flaskecgmod.py
@@ -1,6 +1,4 @@
 
-
-
 def hrmcalculate(time,voltage):
     ecgcalcs = HrmVals(time,voltage)
     try:
@@ -14,7 +12,29 @@ def hrmcalculate(time,voltage):
     return instant_hr, tachycondition, bradycondition
 
 def generateresp(t,instant_hr,tachycondition,bradycondition):
+    return_message = {"time":t, "instantaneous_heart_rate":instant_hr,
+                    "tachycardia_annotations":tachycondition,
+                    "bradycardia_annotations":bradycondition}
+    return jsonify(return_message)
 
+def hrmcalculateave(time, voltage, average_window):
+    ecgcalcs = HrmVals(time,voltage)
+    try:
+        ecgcalcs.hrm_data()
+    except ValueError:
+        print("No heartbeats detected")
+    ecgcalcs.hrm_average_data(average_window)
+    average_hr = ecgcalcs.average_hr
+    tachycondition = ecgcalcs.tachy
+    bradycondition = ecgcalcs.brady
+    return average_hr,tachycondition, bradycondition
+
+def generaterespave(t,average_window,average_hr,tachycondition, bradycondition):
+    return_message = {"averaging_period": average_window, "time": t,
+                      "average_heart_rate": average_hr,
+                      "tachycardia_annotations": tachycondition,
+                      "bradycardia_annotations": bradycondition}
+    return jsonify(return_message)
 
 
 @app.route("/api/heart_rate/summary", methods=['POST'])
@@ -35,5 +55,23 @@ def hrsummary():
     except ValidationError
         print("Response not correct ")
 
+
+@app.route("/api/heart_rate/average", methods=['POST'])
+def hraverage():
+    import numpy as np
+    global countave
+    countave += 1
+    try:
+        validateave(data)
+    except ValidationError as inst:
+        return send_error(inst), 400
+    try:
+        hrmcalculateave(time,voltage,average_window)
+    except ValueError:
+        print("No heartbeats detected")
+    try:
+        generaterespave(t,average_window, average_hr, tachycondition, bradycondition)
+    except ValidationError:
+        print("Response not correct ")
 
 
